bd365c303ccb92e6bccddb6a6f52da5106174e77
Matheus Barni <matheusbarni@Mac-mini-de-Giovani.local>
2021-02-16 08:13:26 -0300

    generating the credit card

--- src/index.js
+++ src/index.js
@@ -1,32 +1,53 @@
-'strict';
+// use puppeteer: https://stackoverflow.com/questions/47060534/how-do-post-request-in-puppeteer
 
 const FormData = require('form-data');
-const axios = require('axios');
 const cheerio = require('cheerio');
-
-// use puppeteer: https://stackoverflow.com/questions/47060534/how-do-post-request-in-puppeteer
+const puppeteer = require('puppeteer');
 
 const baseUrl = 'https://www.4devs.com.br';
 const userAgent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 11_0_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36';
-const Headers = { 
-  'User-Agent': userAgent,
-  'Content-Type': 'multipart/form-data;',
-  'Accept': '*/*',
-  'origin': 'https://www.4devs.com.br'
-};
 
 const params = new FormData();
 params.append('acao', 'gerar_cc');
 params.append('pontuacao', 'S');
 params.append('bandeira', 'master');
 
-const getHtml = async () => {
-  const forDevResponse = await axios.post(`${baseUrl}/ferramentas_online.php`, params, { Headers });
-  console.log(forDevResponse);
-};
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
 
-function main() {
-  getHtml();
-}
+  await page.setRequestInterception(true);
 
-main();
+  page.once("request", interceptedRequest => {
+    interceptedRequest.continue({
+      method: "POST",
+      postData: "acao=gerar_cc&pontuacao=S&bandeira=master",
+      headers: {
+        ...interceptedRequest.headers(),
+        "Content-Type": "application/x-www-form-urlencoded"
+      }
+    });
+  });
+
+  const response = await page.goto(`${baseUrl}/ferramentas_online.php`);
+  const html = await response.text();
+
+  await browser.close();
+
+  const $ = cheerio.load(html);
+
+  console.log('html', html);
+  console.log('$', $);
+
+  const numeroCartao = $('div#cartao_numero').text();
+  const dataValidadeCartao = $('div#data_validade').text();
+  const codigoSegurancaCartao = $('div#codigo_seguranca').text();
+
+  console.log('cartao', {
+    numeroCartao,
+    dataValidadeCartao,
+    codigoSegurancaCartao
+  });
+
+})();
+
